a483f8081c38e7d066946e9bdca1fa01e37fd602
M-Bora-B <mehmetborabocekoglu@gmail.com>
2024-11-26 14:13:16 +0300

    a

---
+++ src/com/esezak/server/Database/Init/DB_Init.java
@@ -0,0 +1,71 @@
+package com.esezak.server.Database.Init;
+import com.esezak.server.Database.Connect.ConnectDB;
+import java.sql.*;
+
+public class DB_Init {
+    public static void main(String[] args) {
+        ConnectDB db = new ConnectDB();
+        Connection connection = db.getConnection();
+        if (connection != null) {
+            try {
+                Statement statement = connection.createStatement();
+                String createUsersTable = """
+            CREATE TABLE IF NOT EXISTS Users (
+                username TEXT PRIMARY KEY,
+                password TEXT NOT NULL
+            );
+        """;
+
+                String createMoviesTable = """
+            CREATE TABLE IF NOT EXISTS Movies (
+                movie_id INTEGER PRIMARY KEY,
+                title TEXT NOT NULL,
+                release_year INTEGER,
+                genre TEXT,
+                director TEXT,
+                overview TEXT,
+                image_url TEXT,
+                rating REAL
+            );
+        """;
+
+                String createReviewsTable = """
+            CREATE TABLE IF NOT EXISTS Reviews (
+                username TEXT,
+                movie_id INTEGER,
+                comment TEXT,
+                user_rating REAL,
+                review_date TEXT,
+                PRIMARY KEY (username, movie_id),
+                FOREIGN KEY (username) REFERENCES Users(username),
+                FOREIGN KEY (movie_id) REFERENCES Movies(movie_id)
+            );
+        """;
+
+                String createWatchlistTable = """
+            CREATE TABLE IF NOT EXISTS Watchlist (
+                username TEXT,
+                movie_id INTEGER,
+                date_added TEXT,
+                user_rating REAL,
+                status TEXT,
+                PRIMARY KEY (username, movie_id),
+                FOREIGN KEY (username) REFERENCES Users(username),
+                FOREIGN KEY (movie_id) REFERENCES Movies(movie_id)
+            );
+        """;
+                statement.executeUpdate(createUsersTable);
+                statement.executeUpdate(createMoviesTable);
+                statement.executeUpdate(createReviewsTable);
+                statement.executeUpdate(createWatchlistTable);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                db.closeConnection();
+            }
+        } else {
+            System.out.println("Connection failed.");
+        }
+    }
+
+}
